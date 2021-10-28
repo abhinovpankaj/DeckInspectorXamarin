60136c2ea19aedd33ca91d4f2474fe5f29ae334b
Abhinov <Abhinovpankaj1@gmail.com>
2021-10-28 20:09:30 +0530

    Offline capability for project creation

--- Mobile.Code/Mobile.Code/Services/SQLiteLocal/BuildingCommonLocationImagesSqLiteDataStore.cs
+++ Mobile.Code/Mobile.Code/Services/SQLiteLocal/BuildingCommonLocationImagesSqLiteDataStore.cs
@@ -1,41 +1,119 @@
 ﻿using Mobile.Code.Models;
+using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Mobile.Code.Services.SQLiteLocal
 {
     public class BuildingCommonLocationImagesSqLiteDataStore : IBuildingCommonLocationImages
     {
+        List<BuildingCommonLocationImages> items;
+        private SQLiteConnection _connection;
+        
+        public BuildingCommonLocationImagesSqLiteDataStore()
+        {
+            _connection = DependencyService.Get<ISQLite>().GetConnection();
+            _connection.CreateTable<BuildingCommonLocationImages>();
+            items = new List<BuildingCommonLocationImages>(); 
+        }
         public Task<bool> AddItemAsync(BuildingCommonLocationImages item)
         {
-            throw new NotImplementedException();
+            items.Add(item);
+            bool status = false;
+            Response res = new Response(); //not being used now.
+            try
+            {
+                var buildingApartment = new BuildingCommonLocationImages
+                {
+                    BuildingLocationId = item.BuildingLocationId,
+                    
+                    UserId = App.LogUser.Id.ToString(),
+                    ImageName = item.ImageName,
+                    ImageUrl = item.ImageUrl,
+                    ImageDescription = item.ImageDescription
+                };
+
+                res.TotalCount = _connection.Insert(buildingApartment);
+                SQLiteCommand Command = new SQLiteCommand(_connection);
+
+                Command.CommandText = "select last_insert_rowid()";
+
+                Int64 LastRowID64 = Command.ExecuteScalar<Int64>();
+
+                res.ID = LastRowID64.ToString();
+
+                res.Message = "Record Inserted Successfully";
+                res.Status = ApiResult.Success;
+                status = true;
+
+            }
+            catch (Exception ex)
+            {
+                res.Message = "Insertion failed." + ex.Message;
+                res.Status = ApiResult.Fail;
+
+            }
+            return Task.FromResult(status);
         }
 
         public Task<Response> DeleteItemAsync(BuildingCommonLocationImages item)
         {
-            throw new NotImplementedException();
+            Response res = new Response();
+            try
+            {
+                _connection.Delete<BuildingCommonLocationImages>(item.Id);
+
+                res.Message = "Record Deleted Successfully";
+                res.Status = ApiResult.Success;
+
+            }
+            catch (Exception)
+            {
+                res.Message = "Deletion Failed";
+                res.Status = ApiResult.Fail;
+            }
+
+            return Task.FromResult(res);
         }
 
         public Task<BuildingCommonLocationImages> GetItemAsync(string id)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(_connection.Table<BuildingCommonLocationImages>().FirstOrDefault(t => t.Id == id));
         }
 
         public Task<IEnumerable<BuildingCommonLocationImages>> GetItemsAsync(bool forceRefresh = false)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(
+               from t in _connection.Table<BuildingCommonLocationImages>()
+               select t
+                   );
         }
 
         public Task<IEnumerable<BuildingCommonLocationImages>> GetItemsAsyncByBuildingId(string BuildingId)
         {
-            throw new NotImplementedException();
+            items = (from t in _connection.Table<BuildingCommonLocationImages>().Where(x => x.BuildingLocationId == BuildingId)
+                     select t).ToList();
+            return Task.FromResult(items.AsEnumerable());
         }
 
         public Task<bool> UpdateItemAsync(BuildingCommonLocationImages item)
         {
-            throw new NotImplementedException();
+            bool result;
+            try
+            {
+                _connection.Update(item);
+                result = true;
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            return Task.FromResult(result);
         }
     }
 }
--- Mobile.Code/Mobile.Code/Services/SQLiteLocal/ProjectSqLiteDataStore.cs
+++ Mobile.Code/Mobile.Code/Services/SQLiteLocal/ProjectSqLiteDataStore.cs
@@ -1,10 +1,127 @@
-﻿using System;
+﻿using Mobile.Code.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Mobile.Code.Services.SQLiteLocal
 {
-    class ProjectSqLiteDataStore
+    public class ProjectSqLiteDataStore : IProjectDataStore
     {
+        List<Project> items;
+        private SQLiteConnection _connection;
+
+        public ProjectSqLiteDataStore()
+        {
+
+            items = new List<Project>();
+            _connection = DependencyService.Get<ISQLite>().GetConnection();
+            _connection.CreateTable<Project>();
+
+        }
+        public Task<Response> AddItemAsync(Project item)
+        {
+
+            items.Add(item);
+           
+            Response res = new Response(); //not being used now.
+            try
+            {
+                var project = new Project
+                {
+                    Name = item.Name,
+                    Address = item.Address,
+                    Description = item.Description,
+                    ProjectType = item.ProjectType,
+                    UserId = App.LogUser.Id.ToString(),
+                    ImageDescription = item.ImageDescription,
+                    ImageName = item.ImageName,
+                    ImageUrl = item.ImageUrl,
+                    Category = item.Category
+               
+            };
+
+                res.TotalCount = _connection.Insert(project);
+                SQLiteCommand Command = new SQLiteCommand(_connection);
+
+                Command.CommandText = "select last_insert_rowid()";
+
+                Int64 LastRowID64 = Command.ExecuteScalar<Int64>();
+
+                res.ID = LastRowID64.ToString();
+
+                res.Message = "Record Inserted Successfully";
+                res.Status = ApiResult.Success;
+                
+
+            }
+            catch (Exception ex)
+            {
+                res.Message = "Insertion failed." + ex.Message;
+                res.Status = ApiResult.Fail;
+
+            }
+            return Task.FromResult(res);
+            
+        }
+
+        public Task<Response> CreateInvasiveReport(Project item)
+        {
+            return Task.FromResult(new Response());
+        }
+
+        public Task<Response> DeleteItemAsync(Project item)
+        {
+            Response res = new Response();
+            try
+            {
+                _connection.Delete<Project>(item.Id);
+
+                res.Message = "Record Deleted Successfully";
+                res.Status = ApiResult.Success;
+
+            }
+            catch (Exception)
+            {
+                res.Message = "Deletion Failed";
+                res.Status = ApiResult.Fail;
+            }
+
+            return Task.FromResult(res);
+        }
+
+        public Task<Project> GetItemAsync(string id)
+        {
+            return Task.FromResult(_connection.Table<Project>().FirstOrDefault(t => t.Id == id));
+        }
+
+        public Task<IEnumerable<Project>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return Task.FromResult(
+              from t in _connection.Table<Project>()
+              select t
+                  );
+        }
+
+        public Task<Response> UpdateItemAsync(Project item)
+        {
+            Response res = new Response();
+            try
+            {
+                _connection.Update(item);
+                res.Message = "Record Updated Successfully";
+                res.Status = ApiResult.Success;
+            }
+            catch (Exception)
+            {
+                res.Message = "Updation Failed";
+                res.Status = ApiResult.Fail;
+            }
+
+            return Task.FromResult(res);
+        }
     }
 }
