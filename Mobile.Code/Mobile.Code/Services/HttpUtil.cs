709682f0fdb41f83e76998ee00e4e97447708d27
priyanka verma <priyanka2050@outlook.com>
2022-09-10 10:14:24 +0530

    Small build issue fixes after merge.

--- Mobile.Code/Mobile.Code/Services/HttpUtil.cs
+++ Mobile.Code/Mobile.Code/Services/HttpUtil.cs
@@ -239,20 +239,7 @@ namespace Mobile.Code.Services
             return await Task.FromResult(returnImage);
         }
 
-        private static string RemoveSpecialChars(string str)
-        {
-            // Create  a string array and add the special characters you want to remove
-            string[] chars = new string[] { ",", ".", "/", "!", "@", "#", "$", "%", "^", "&", "*", "'", "\"", ";", "_", "(", ")", ":", "|", "[", "]" };
-            //Iterate the number of times based on the String array length.
-            for (int i = 0; i < chars.Length; i++)
-            {
-                if (str.Contains(chars[i]))
-                {
-                    str = str.Replace(chars[i], "");
-                }
-            }
-            return str;
-        }
+        
 
     }
 }
